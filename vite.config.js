3a12866bc996a289cc14d92ab14ba66af56bd587
Areeb Azhar <areebazhar3@gmail.com>
2026-04-04 02:50:10 +0500

    version 2.1
    fix animations

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/Portfolio/', // Adjust this to match your repo name exactly
+  build: {
+    outDir: 'dist',
+  },
+});
